1506f8359a4f715e0dc43cc35c274469e2f81cbc
Gotame <yamugotame@gmail.com>
2016-04-11 11:54:02 -0400

    Update custom.js

--- assets/js/soon/custom.js
+++ assets/js/soon/custom.js
@@ -6,7 +6,7 @@ COMMING SOON PAGE
     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
     * according to the GMT+0 Timezone
     **/
-    var launch = new Date(2016, 03, 11, 11, 11);
+    var launch = new Date(2016, 11, 11, 11, 11);
     /**
     * The script
     **/
